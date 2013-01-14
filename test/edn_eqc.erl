106b1207fea697f1c15eda9cc2be132087cee876
Sean Cribbs <sean@basho.com>
2013-01-14 14:07:11 -0700

    Added some more failing properties, symbol rule is still buggy.

--- test/edn_eqc.erl
+++ test/edn_eqc.erl
@@ -16,6 +16,25 @@
 %% Generators
 %% -------------------------------------
 
+%% Generates a list
+gen_list() ->
+    ?LET({Open, Elements, Close},
+         {ws_wrap("("), ws_joined_list(gen_element()), ws_wrap(")")},
+         ?to_utf8([Open, Elements, Close])).
+
+%% Generates an arbitrary edn term
+gen_element() ->
+   oneof([
+          gen_float(),
+          gen_integer(),
+          gen_char(),
+          gen_keyword(),
+          gen_string(),
+          gen_bool(),
+          gen_nil(),
+          gen_symbol()
+         ]).
+
 %% Generates a floating point number
 gen_float() ->
     ?LET({Float, Mark},
@@ -24,7 +43,7 @@ gen_float() ->
 
 %% Generates an integer
 gen_integer() ->
-    elements([gen_int(), gen_arb_int()]).
+    ?LET(I, elements([gen_int(), gen_arb_int()]), I).
 
 %% Generates an integer with the arbitrary precision marker
 gen_arb_int() ->
@@ -58,9 +77,13 @@ gen_escape_char() ->
 
 %% Borrowed from erlang_protobuffs, generates a valid UTF8 char
 utf8_char() ->
-    elements([choose(0, 36095),
-              choose(57344, 65533),
-              choose(65536, 1114111)]).
+    ?LET(C, elements([choose(0, 36095),
+                      choose(57344, 65533),
+                      choose(65536, 1114111)]), C).
+
+%% Generates a keyword
+gen_keyword() ->
+    ?LET(Sym, gen_symbol(), ?to_utf8([$:, Sym])).
 
 %% Generates a symbol
 gen_symbol() ->
@@ -73,8 +96,8 @@ gen_leading_slash_symbol() ->
 %% Generates a normal symbol, i.e. one without a leading slash
 gen_normal_symbol() ->
     frequency([{2, gen_simple_symbol()},
-           {1, gen_trailing_slash_symbol()},
-           {2, gen_embedded_slash_symbol()}]).
+               {1, gen_trailing_slash_symbol()},
+               {2, gen_embedded_slash_symbol()}]).
 
 %% Generates a symbol that doesn't contain a slash
 gen_simple_symbol() ->
@@ -99,24 +122,24 @@ gen_embedded_slash_symbol() ->
 %% Generates a non-numeric character that is valid within a symbol
 gen_non_numeric() ->
     oneof([$., $$, $+, $!, $-, $_, $?] ++
-          lists:seq($A, $Z) ++
-          lists:seq($a, $z)).
+              lists:seq($A, $Z) ++
+              lists:seq($a, $z)).
 
 %% Generates a non-numeric character that is valid within a symbol,
 %% but with the addition of # and : which are valid when not in first
 %% position.
 gen_non_numeric_with_punct() ->
     oneof([$., $$, $+, $!, $-, $_, $?, $#, $:] ++
-          lists:seq($A, $Z) ++
-          lists:seq($a, $z)).
+              lists:seq($A, $Z) ++
+              lists:seq($a, $z)).
 
 %% Generates a character that is valid within a symbol after the first
 %% and second positions.
 gen_symbol_char() ->
     oneof([$., $$, $+, $!, $-, $_, $?, $#, $:] ++
-          lists:seq($A, $Z) ++
-          lists:seq($a, $z) ++
-          lists:seq($0, $9)).
+              lists:seq($A, $Z) ++
+              lists:seq($a, $z) ++
+              lists:seq($0, $9)).
 
 %% Generates 'nil'.
 gen_nil() ->
@@ -138,6 +161,18 @@ ws_wrap(Gen) ->
          {gen_ws(), Gen, gen_ws()},
          ?to_utf8([LWS, V, TWS])).
 
+%% Generates a list of the input type, joined with non-empty
+%% whitespace.
+ws_joined_list(Type) ->
+    ?LET({LWS, List}, 
+         {gen_ws(), list({ws_wrap(Type), non_empty(gen_ws())})},
+         case length(List) of
+             0 -> 
+                 ?to_utf8([LWS]);
+             _ ->
+                 ?to_utf8([LWS | [ tuple_to_list(Pair) || Pair <- List ]])
+         end).
+
 %% ---------------------------------------------------
 %% Parser properties (edn docs copy-pasted above each)
 %% ---------------------------------------------------
@@ -148,7 +183,7 @@ ws_wrap(Gen) ->
 %% other than within strings.
 prop_whitespace() ->
     ?FORALL(Spaces, gen_ws(),
-                    ok == ?assertThrow({edn,empty}, parse(Spaces))).
+            ok == ?assertThrow({edn,empty}, parse(Spaces))).
 
 %% true and false should be mapped to booleans.
 prop_bool() ->
@@ -171,18 +206,18 @@ prop_string() ->
     ?FORALL(String, ws_wrap(gen_string()),
             is_binary(parse(String))).
 
-% Symbols are used to represent identifiers, and should map to
-% something other than strings, if possible.
-%
-% Symbols begin with a non-numeric character and can contain
-% alphanumeric characters and . * + ! - _ ?. If - or . are the first
-% character, the second character must be non-numeric. Additionally, :
-% # are allowed as constituent characters in symbols but not as the
-% first character.
-%
-% / has special meaning in symbols. It can be used once only in the
-% middle of a symbol to separate the prefix (often a namespace) from
-% the name, e.g. my-namespace/foo. / by itself is a legal symbol.
+%% Symbols are used to represent identifiers, and should map to
+%% something other than strings, if possible.
+%%
+%% Symbols begin with a non-numeric character and can contain
+%% alphanumeric characters and . * + ! - _ ?. If - or . are the first
+%% character, the second character must be non-numeric. Additionally, :
+%% # are allowed as constituent characters in symbols but not as the
+%% first character.
+%%
+%% / has special meaning in symbols. It can be used once only in the
+%% middle of a symbol to separate the prefix (often a namespace) from
+%% the name, e.g. my-namespace/foo. / by itself is a legal symbol.
 prop_symbol() ->
     ?FORALL(Symbol, ws_wrap(non_empty(gen_symbol())),
             is_atom(parse(Symbol))).
@@ -208,4 +243,39 @@ prop_integer() ->
 prop_float() ->
     ?FORALL(Float, ws_wrap(gen_float()),
             is_float(parse(Float))).
+
+%% Keywords are identifiers that typically designate themselves. They
+%% are semantically akin to enumeration values. Keywords follow the
+%% rules of symbols, except they can (and must) begin with a colon,
+%% e.g. :fred or :my/fred. If the target platform does not have a
+%% keyword type distinct from a symbol type, the same type can be used
+%% without conflict, since the mandatory leading : of keywords is
+%% disallowed for symbols.
+%%
+%% If the target platform supports some notion of interning, it is a
+%% further semantic of keywords that all instances of the same keyword
+%% yield the identical object.
+prop_keyword() ->
+    ?FORALL(Keyword, 
+            ws_wrap(gen_keyword()),
+            begin
+                Parsed = parse(Keyword),
+                is_atom(Parsed) andalso hd(atom_to_list(Parsed)) =:= $:
+            end).
+
+
+%% A list is a sequence of values. Lists are represented by zero or
+%% more elements enclosed in parentheses (). Note that lists can be
+%% heterogeneous.
+prop_list() ->
+    ?FORALL(List, ws_wrap(gen_list()),
+            is_list(parse(List))).
+
+%% A vector is a sequence of values that supports random access.
+%% Vectors are represented by zero or more elements enclosed in square
+%% brackets []. Note that vectors can be heterogeneous.
+
+%% prop_vector() ->
+%%     ?FORALL(Vector, ws_wrap(gen_vector()),
+%%             is_tuple(parse(Vector))).
 -endif.
